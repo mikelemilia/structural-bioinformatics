37a41b9ae134e91abdb6287fdd94c286782071cf
giuliapezzutti <74766522+giuliapezzutti@users.noreply.github.com>
2021-05-28 09:13:55 +0200

    Some prints

--- scripts/midterm-2/main.py
+++ scripts/midterm-2/main.py
@@ -6,7 +6,7 @@ from functions import *
 if __name__ == "__main__":
 
     # IUPRED prediction
-    pdb_ids = [('2loj', 'A'), ('2m1s', 'A'), ('2kkw', 'A')]
+    pdb_ids = [('2loj', 'A'), ('2m1s', 'A'), ('2kkw', 'A'), ('1tac', 'A')]
 
     pdbl = PDBList()
     for pdb_id, chain_id in pdb_ids:
@@ -44,12 +44,10 @@ if __name__ == "__main__":
 
             exact_energy.append(energy_temp)
 
-
         # Smooth the prediction (moving average)
         for i in range(len(exact_energy)):
             frag = exact_energy[max(0, i - 10): min(i + 10, len(exact_energy))]
             exact_energy_smooth.append(sum(frag) / len(frag))
-
 
         fig, ax = plt.subplots(figsize=(12, 6))
         ax.set_title("{}_{}".format(pdb_id, chain_id))
@@ -61,27 +59,30 @@ if __name__ == "__main__":
         plt.savefig('output/midterm-2/mypred_{}_{}.png'.format(pdb_id, chain_id), bbox_inches='tight')
 
         # Punto 3
+        pred_energy, pred_energy_smooth = iupred(seq)  # prediction
+
         fig, ax = plt.subplots(figsize=(12, 6))
-        pred, pred_smooth = iupred(seq)  # prediction
         ax.set_title("{}_{}".format(pdb_id, chain_id))
         ax.axhline()
-        ax.plot(np.arange(len(seq)), pred, ls='--')
-        ax.plot(np.arange(len(seq)), pred_smooth, ls='-')
+        ax.plot(np.arange(len(seq)), pred_energy, ls='--')
+        ax.plot(np.arange(len(seq)), pred_energy_smooth, ls='-')
 
         plt.tight_layout()  # Remove figure padding
         plt.savefig('output/midterm-2/iupred_{}_{}.png'.format(pdb_id, chain_id), bbox_inches='tight')
 
         # Punto 4
-        count = np.sum(np.array(pred_smooth) >= 0, axis=0)
-        total = len(pred_smooth)
-        ratio = count / total
-        print(count, total, ratio)
-        print(pred_smooth)
 
+        count_exact = np.sum(np.array(exact_energy_smooth) >= 0, axis=0)
+        total_exact = len(exact_energy_smooth)
+        ratio_exact = count_exact / total_exact
+        print("Disorder content for exact energy: ", count_exact, total_exact, ratio_exact)
+        # print(exact_energy_smooth)
 
-        count2 = np.sum(np.array(exact_energy_smooth) >= 0, axis=0)
-        total2 = len(exact_energy_smooth)
-        ratio2 = count2 / total2
-        print(count2, total2, ratio2)
-        print(exact_energy_smooth)
+        count_pred = np.sum(np.array(pred_energy_smooth) >= 0, axis=0)
+        total_pred = len(pred_energy_smooth)
+        ratio_pred = count_pred / total_pred
+        print("Disorder content for predicted energy: ", count_pred, total_pred, ratio_pred)
+        # print(pred_smooth)
+
+        print("\n")
 
